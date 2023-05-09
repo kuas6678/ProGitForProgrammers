6b7861117bf4d6e2bb6c2b5b030e365af0f5437a
莊英澔 <kuas6678@gmail.com>
2023-05-09 23:39:30 +0800

    ADd the subtract method

--- ProGitForProgrammers/Calculator.cs
+++ ProGitForProgrammers/Calculator.cs
@@ -11,5 +11,10 @@ namespace ProGitForProgrammers
         {
             return left + right;
         }
+
+        public int Subtract(int left, int right)
+        {
+            return left - right;
+        }
     }
 }
