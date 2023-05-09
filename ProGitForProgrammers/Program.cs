c1b410dfafb3f6fdb864b9e7bcbba4f3c87979bb
莊英澔 <kuas6678@gmail.com>
2023-05-09 23:16:19 +0800

    add calculator class

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -6,8 +6,8 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Add in Visual studio");
-            Console.WriteLine("This line add to show the staging area");
             Console.WriteLine("add this message from Visual Studio");
+            Console.WriteLine("This line add to show the staging area");
         }
     }
 }
