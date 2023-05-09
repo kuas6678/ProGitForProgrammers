a6a51a3ffc113fc3a715bd8e24a4a0a2ba742656
莊英澔 <kuas6678@gmail.com>
2023-05-09 23:32:54 +0800

    Add Book class Property

--- ProGitForProgrammers/Book.cs
+++ ProGitForProgrammers/Book.cs
@@ -8,6 +8,10 @@ namespace ProGitForProgrammers
 {
     public class Book
     {
+        public string Title { get; set; }
 
+        public List<string> Authors { get; set; }
+
+        public DateTime PublicationDate { get; set; }
     }
 }
