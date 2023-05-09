254ad0e3f56149d087f62168d5ff513741ad9459
莊英澔 <kuas6678@gmail.com>
2023-05-09 22:49:34 +0800

    Commit From VS

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Add in Visual studio");
+            Console.WriteLine("add this message from Visual Studio");
         }
     }
 }
